4ce61b90707b6e34ec8be3ad2f293521b1da4a34
Nyakote <dim4ka228@ukr.net>
2024-04-04 17:26:32 +0300

    Pow operation and teswt to it

---
+++ Advanced_test/Calculator_Advanced.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Advanced_test
+{
+    public class Calculator_Advanced
+    {
+        public int Pow(int a, int b) { for (int i = 0; i < b;) { a *= i; } return a; }
+        
+    }
+}
